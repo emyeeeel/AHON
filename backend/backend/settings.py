e6db267a708baf86b0a78e71c74e3c589f1caf27
Emyeeeel <maryamielcatado@gmail.com>
2025-07-25 16:55:37 +0800

    added needed files for stream

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'detection',
+    'locator',
+    'mission',
+    'stream',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
