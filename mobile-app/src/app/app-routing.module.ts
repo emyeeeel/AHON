4ae75bf922827b454f07b0445303f0f5f3f943b3
AJ <anthonyjamesplaza@gmail.com>
2025-07-25 15:46:45 +0800

    initialized tabs w/ history and settings pages

--- mobile-app/src/app/app-routing.module.ts
+++ mobile-app/src/app/app-routing.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
+   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'splash',
     pathMatch: 'full'
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'splash',
+    loadChildren: () => import('./pages/splash/splash.module').then( m => m.SplashPageModule)
+  },
+
 ];
 
 @NgModule({
