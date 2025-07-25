a85a4ee148acefe85c37b5d19b8055bb5bad866e
Emyeeeel <maryamielcatado@gmail.com>
2025-07-25 16:23:36 +0800

    added login page

--- web-app/src/app/pages/auth-pages/login/login.component.ts
+++ web-app/src/app/pages/auth-pages/login/login.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  onLogin() {
+    // TODO: Implement actual login logic
+    console.log('Login submitted');
+  }
 }
