3d6b9c41a4833ff7d2936997a20342e23ceef66c
Marylle Roche <rochefym@gmail.com>
2025-07-25 16:44:59 +0800

    Created stream & victims page

---
+++ mobile-app/src/app/pages/tabs/victims/victims.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-victims',
+  templateUrl: './victims.page.html',
+  styleUrls: ['./victims.page.scss'],
+})
+export class VictimsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
