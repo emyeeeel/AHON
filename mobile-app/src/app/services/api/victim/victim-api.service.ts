4df8f1d8cfde44a4145b2a786ff531ba8231c965
Marylle Roche <rochefym@gmail.com>
2025-07-26 00:15:10 +0800

    Updated API services of mission & victim

--- mobile-app/src/app/services/api/victim/victim-api.service.ts
+++ mobile-app/src/app/services/api/victim/victim-api.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VictimApiService {
+  baseUrl = 'http://172.29.14.173:8000/api';
+  httpHeaders = { 'Content-Type': 'application/json' };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createVictim(victim: any) {
+    return this.http.post(`${this.baseUrl}/victims/`, victim, { headers: this.httpHeaders });
+  }
+
+  updateVictims(victim: any) {
+    return this.http.put(`${this.baseUrl}/mission/${mission.id}/victims/`, mission, { headers: this.httpHeaders });
+  }
+
+  getVictimsByMission(missionId: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/mission/${missionId}/victims/`, { headers: this.httpHeaders });
+  }
 }
--- mobile-app/src/app/services/api/victim/victim-response.service.ts
+++ mobile-app/src/app/services/api/victim/victim-response.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { VictimApiService } from './victim-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VictimResponseService {
+  constructor(
+    private victimApiService: VictimApiService
+  ) { }
 
-  constructor() { }
+
+  async getVictimsByMission(missionId: number): Promise<any> {
+    try {
+      const response = await this.victimApiService.getVictimsByMission(missionId).toPromise();
+      return response;
+    } catch (error) {
+      throw new Error('Failed to get victims by mission');
+    }
+  }
 }
